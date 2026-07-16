7c81b356a524c64c090a7996fe54f6a61d8ace3b
DiegoAlanya <diego@neoportfolio.com>
2026-07-16 03:35:48 -0500

    host publico Railway

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,9 @@
 FROM tomcat:9.0-jdk11
+
+# Descargar conector MySQL
+RUN curl -o /usr/local/tomcat/lib/mysql-connector.jar https://repo1.maven.org/maven2/com/mysql/mysql-connector-j/8.0.33/mysql-connector-j-8.0.33.jar
+
 COPY dist/NeoPortfolio.war /usr/local/tomcat/webapps/ROOT.war
+
 EXPOSE 8080
 CMD ["catalina.sh", "run"]
--- src/java/modelo/ConexionBD.java
+++ src/java/modelo/ConexionBD.java
@@ -5,16 +5,16 @@ import java.sql.DriverManager;
 
 public class ConexionBD {
     
-    private static final String URL = "jdbc:mysql://sql10833156.freesqldatabase.com:3306/sql10833156?useSSL=false&serverTimezone=UTC";
-    private static final String USER = "sql10833156";
-    private static final String PASS = "Tw4sJ3GlXy";
+    private static final String URL = "jdbc:mysql://tokaido.proxy.rlwy.net:17686/railway?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASS = "TyYcNUcOAoPabLfxQNUCEZVqjcIMRZRw";
     
     public static Connection getConexion() {
         Connection conn = null;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             conn = DriverManager.getConnection(URL, USER, PASS);
-            System.out.println("✅ Conectado a hosting MySQL");
+            System.out.println("✅ Conectado a Railway MySQL");
         } catch (Exception e) {
             System.err.println("❌ Error: " + e.getMessage());
         }
